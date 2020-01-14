5791d6ade2f28d8344006b42d2e68d83e702fd05
Jon <jonathanjackson424@gmail.com>
2020-01-14 16:13:14 -0500

    fixed README image

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+import Homepage from './Homepage/Homepage';
+import MajorScalesAndKeySignaturesQuestions from './Questions/MajorScalesAndKeySignaturesQuestions';
+import PitchQuizQuestions from './Questions/PitchQuizQuestions';
+import MajorScalesAndKeySignaturesQuiz from './Quiz/MajorScalesAndKeySignaturesQuiz';
+import PitchQuiz from './Quiz/PitchQuiz';
+import Quizzes from './Quizzes/Quizzes';
+import MajorScalesAndKeySignatures from './Topic/MajorScalesAndKeySignatures';
+import Pitch from './Topic/Pitch';
+import Topics from './Topics/Topics';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+describe ('Topics component', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Topics />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
 });
